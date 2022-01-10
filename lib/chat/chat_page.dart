3869af23f606209cbeeb00b14f0121929d70f6c5
kouchiryunosuke <azukimankun@gmail.com>
2022-01-10 17:46:27 +0900

    UI完成　個人チャット実装をする

--- lib/chat/chat_page.dart
+++ lib/chat/chat_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//チャット機能
 class ChatPage extends StatefulWidget {
   ChatPage(this._userName);
 
