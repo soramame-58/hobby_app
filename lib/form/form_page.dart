a4ac53e2a4ea68e76378469c4e61444378644554
kouchiryunosuke <azukimankun@gmail.com>
2022-01-10 18:33:39 +0900

    googleフォームでのお問い合わせ作成

--- lib/form/form_page.dart
+++ lib/form/form_page.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+//お問い合わせ
 class FormPage extends StatefulWidget {
   @override
   FormPageState createState() => FormPageState();
